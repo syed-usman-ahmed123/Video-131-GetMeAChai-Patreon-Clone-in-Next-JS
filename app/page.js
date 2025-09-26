ef8a124e54758d27502ac91a8b635fb8fd36bb81
syed-usman-ahmed123 <usmanrdx903@gmail.com>
2025-09-26 16:19:22 -0700

    create authentication

--- app/page.js
+++ app/page.js
@@ -10,8 +10,8 @@ export default function Home() {
 
         <p>A crowdfunding platform for creators. Get funded by your fans and followers. Start now!</p>
         <div>
-          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here </button>
-          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here </button>
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
         </div>
       </div>
       <div className="bg-white h-1 opacity-10"></div>
@@ -40,7 +40,7 @@ export default function Home() {
       <div className="bg-white h-1 opacity-10"></div>
       <div className="text-white container mx-auto pb-32 pt-14 flex flex-col items-center justify-center">
         <h2 className="text-3xl font-bold text-center mb-14 ">Learn more about us</h2>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/QtaorVNAwbI?si=mLdqiJQmP-1tVYxI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe width="315" height="576" src="https://www.youtube.com/embed/Q6LBcBm2jDs" title="Benefits of Coding 🔥🔥" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </>
   );
--- components/navbar.js
+++ components/navbar.js
@@ -1,34 +1,70 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
 import Link from 'next/link'
 
-const Navbar =() => {
-    const { data: session } = useSession()
-      if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
+const Navbar = () => {
+  const { data: session, } = useSession()
+  console.log("Session data:", session)
+  const [showdropdown, setShowdropdown] = useState(false)
+
+  //     if(session) {
+  //   return <>
+  //     Signed in as {session.user.email} <br/>
+  //     <button onClick={() => signOut()}>Sign out</button>
+  //   </>
+  // }
   return (
     <nav className='bg-slate-900 text-white flex justify-between px-4 h-16 items-center'>
-        <div className="logo font-bold text-lg flex justify-center items-center">
-          <img width={55} src="teal.gif" alt="" />
-          <span>GetMeAChai</span>
-        </div>
-        {/* <ul className='flex justify-between gap-4'>
+      <div className="logo font-bold text-lg flex justify-center items-center">
+        <img width={55} src="teal.gif" alt="" />
+        <span>GetMeAChai</span>
+      </div>
+      {/* <ul className='flex justify-between gap-4'>
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
             <li>Sign Up</li>
             <li>Login</li>
         </ul> */}
-        <div>
-         <Link href={"/login"}>
-          <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button></Link>
-        </div>
-        </nav>
+      <div className='relative'>
+        {session && <>
+          <button onClick={() => setShowdropdown(!showdropdown)} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white  mx-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Welcome {session.user.email} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+          </svg>
+          </button>
+
+          {/* <!-- Dropdown menu --> */}
+          <div id="dropdown" className={`z-10 ${showdropdown ? "" : "hidden"} absolute left-[130px] bg-white divide-y  divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700`}>
+            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+              </li>
+              <li>
+                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+              </li>
+            </ul>
+          </div></>
+        }
+        {session &&
+          <Link href={"/dashboard"}>
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Dashboard</button>
+          </Link>}
+        {session &&
+
+          <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => { signOut() }} >Logout</button>
+        }
+        {!session &&
+          <Link href={"/login"}>
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" >Login</button></Link>}
+      </div>
+    </nav>
   )
 }
 
